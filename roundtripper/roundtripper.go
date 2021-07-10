2aaaa40928b06be2176ffc69a3347292436dd7ad
Jittapon Thamrongsawad <jittapon.t@lmwn.com>
2021-07-10 14:36:48 +0700

    feat: add makeGetRequest in round tripper

--- roundtripper/roundtripper.go
+++ roundtripper/roundtripper.go
@@ -1,8 +1,20 @@
 package roundtripper
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 //go:generate mockgen -destination=./mock_roundtripper.go -package=roundtripper http-mock/roundtripper RoundTripper
 type RoundTripper interface {
 	RoundTrip(*http.Request) (*http.Response, error)
+}
+
+func makeGetRequest(client *http.Client, req *http.Request) (*http.Response, error) {
+	resp, err := client.Do(req)
+	if err != nil {
+		return resp, err
+	}
+	log.Printf("Response status code : %d", resp.StatusCode)
+	return resp, nil
 }
